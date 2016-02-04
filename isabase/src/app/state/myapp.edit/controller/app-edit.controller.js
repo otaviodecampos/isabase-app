774c575d7f2259633a69c2111060f695d2f361db
Otávio de Campos <otaviodecampos@hotmail.com>
2016-02-04 21:09:03 +0000

    Notification updates

--- isabase/src/app/state/myapp.edit/controller/app-edit.controller.js
+++ isabase/src/app/state/myapp.edit/controller/app-edit.controller.js
@@ -5,27 +5,31 @@
         .controller('MyAppEditCtrl', Controller);
 
     /* @ngInject */
-    function Controller(MyApp, $stateParams, Navigation) {
+    function Controller($stateParams, MyApp, Navigation, Notification) {
         var that = this;
         var id = $stateParams.id;
 
         this.myapp = MyApp.get({id: id});
 
         this.save = function() {
-            if(this.myapp.name != undefined) {
+            if(this.myapp.name) {
                 this.myapp.$save(function(app) {
                     Navigation.back({selected: id});
+                    Notification.success('edit-app', that.myapp.name, 'save-success');
                 }, function(e) {
-                    console.log(e);
+                    Notification.error('edit-app', that.myapp.name, 'save-error');
                 });
+            } else {
+                Notification.info('edit-app', that.myapp.name, 'empty-app-name');
             }
         }
         
         this.remove = function() {
             this.myapp.$remove(function() {
                 Navigation.back();
+                Notification.error('edit-app', that.myapp.name, 'remove-success');
             }, function(e) {
-                console.log(e);
+                Notification.error('edit-app', that.myapp.name, 'remove-error');
             });
         }
 
