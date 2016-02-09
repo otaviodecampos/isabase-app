ced4bbef98b9155bc8cce31dd1c94ae39aaeed4d
otaviodecampos <otaviodecampos@hotmail.com>
2016-02-09 05:13:05 -0200

    add animation

--- isabase/src/app/app.run.js
+++ isabase/src/app/app.run.js
@@ -5,11 +5,20 @@
         .run(Run);
 
     /* @ngInject */
-    function Run($rootScope, $stateParams, APP, Navigation) {
+    function Run($rootScope, $state, $stateParams, $timeout, APP, Navigation) {
 
         $rootScope.stateParams = $stateParams;
         $rootScope.app = APP;
         $rootScope.back = Navigation.back;
+
+        $rootScope.$on('$stateChangeStart', function (event, toState) {
+            var back = $state.includes(toState.name);
+            if (back) {
+                $rootScope.back = true;
+            } else {
+                $rootScope.back = false;
+            }
+        });
 
     };
 
